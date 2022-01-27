500f4b237bc83272c0d67850b73fa51412476ed8
pmaurya <pmaurya@splunk.com>
2022-01-27 23:48:51 +0530

    added header and footer component

--- components/Property.jsx
+++ components/Property.jsx
@@ -9,30 +9,30 @@ import DefaultImage from "../assets/images/house.jpg"
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID}}) => {
   return (
-      <Link href={`/property/${externalID }`} passHref>
-          <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-end" cursor="pointer"> 
-            <Box>
-                <Image src={ coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house"/>
-            </Box>
-            <Box w="full">
-                <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
-                    <Flex alignItems="center">
-                        <Box paddingRight="3" color="green.400">{isVerified && <GoVerified/>}</Box>
-                        <Text fontWeight="bold" fontSize="lg">AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
-                    </Flex>
-                    <Box>
-                        <Avatar size="sm" src={agency?.logo?.url}/>
-                    </Box>
+    <Link href={`/property/${externalID }`} passHref>
+        <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-end" cursor="pointer"> 
+        <Box>
+            <Image src={ coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house"/>
+        </Box>
+        <Box w="full">
+            <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+                <Flex alignItems="center">
+                    <Box paddingRight="3" color="green.400">{isVerified && <GoVerified/>}</Box>
+                    <Text fontWeight="bold" fontSize="lg">AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
                 </Flex>
-                <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
-                    {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
-                </Flex>
-                <Text fontSize="lg">
-                   {title.length > 40 ? `${title.substring(0, 30)}...`: title}  
-                </Text>
-            </Box>
-          </Flex>
-      </Link>
+                <Box>
+                    <Avatar size="sm" src={agency?.logo?.url}/>
+                </Box>
+            </Flex>
+            <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
+                {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+            </Flex>
+            <Text fontSize="lg">
+                {title.length > 40 ? `${title.substring(0, 30)}...`: title}  
+            </Text>
+        </Box>
+        </Flex>
+    </Link>
   );
 }
 
