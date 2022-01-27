3573236b5eeeaede44c2e6df808a596df32f7f10
pmaurya <pmaurya@splunk.com>
2022-01-27 19:28:40 +0530

    added banner and axios

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,51 @@
 import Link from "next/link"; 
-import styles from '../styles/Home.module.css'
+import Image from "next/image";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
+
+const Banner = ({purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl}) => {
+  return (
+    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+     <Image src={imageUrl} width={500} height={300} alt="banner"/>
+     <Box p="5">
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+      <Text fontSize="3xl" fontWeight="bold ">{title1}<br />{title2}</Text>
+      <Text fontSize="lg" py="3" color="gray.700">{desc1}<br />{desc2}</Text>
+      <Button fontSize="xl">
+        <Link href={linkName}><a>{buttonText}</a></Link>
+      </Button>
+     </Box>
+   </Flex>
+  )
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      
-    </div>
+    <Box>
+      <h1>Hello World</h1>
+      <Banner 
+        purpose="Rent a home"
+        title1="Rental Homes for"
+        title2="Everyone "
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+      <Flex flexWrap="wrap">
+        {/** Fetch the properties and map over them... */}
+        
+      </Flex>
+      <Banner 
+        purpose="Buy a home"
+        title1="Find , Buy and Own your"
+        title2="Dream Home"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Buying"
+        linkName="/search?purpose=for-sale"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+    </Box>
   )
 }
---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const fetchApi = async (url) => {
+    const {data} = await axios.get(url, {
+        headers: {
+            'x-rapidapi-host': 'bayut.p.rapidapi.com',
+            'x-rapidapi-key': '8fe3293952msh850c11c181291e1p109b37jsn69659ce9f285'
+          }
+    })
+}
