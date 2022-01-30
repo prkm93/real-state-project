e73ff7c9a018f42a0fadebe21ca3dd3209c7d1f9
prkm93 <aradshub93@gmail.com>
2022-01-30 11:13:02 +0530

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
     const {data} = await axios.get(url, {
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '8fe3293952msh850c11c181291e1p109b37jsn69659ce9f285'
+            'x-rapidapi-key': process.ENV.key
           }
     })
 
